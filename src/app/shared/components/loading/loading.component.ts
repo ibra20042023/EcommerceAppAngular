746e9876251c34284c7112fa49e2997ae11ba0ba
Mamady Sylla <mamadysyllafr@gmail.com>
2026-05-10 21:23:26 +0200

    initialisation par base du depot distant

--- src/app/shared/components/loading/loading.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading',
-  templateUrl: './loading.component.html',
-})
-export class LoadingComponent {}
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  API_URL: 'http://localhost:5180/api',
-  TAX_RATE: 13,
-};
